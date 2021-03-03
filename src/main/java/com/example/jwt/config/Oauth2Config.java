949809a04a78397bf0cce45c4b2ac054b19e6386
Cristiano Santos <cris91@hotmail.com.br>
2021-03-03 18:04:00 -0300

    liberando swagger

--- src/main/java/com/example/jwt/config/Oauth2Config.java
+++ src/main/java/com/example/jwt/config/Oauth2Config.java
@@ -3,6 +3,7 @@ package com.example.jwt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,6 +20,17 @@ public class Oauth2Config extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("springboot").password("springboot")
                 .roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**"
+        );
     }
 
     @Bean
